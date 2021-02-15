36418456489982c2d74b32b359b593f9c9f7bc3e
snuzi <sabrinuzi@gmail.com>
2021-02-15 20:30:28 +0100

    The first working search page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
       <Home/>
-    </div>
   );
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,57 @@
 import React from 'react';
-import { InstantSearch, SearchBox, Hits, Highlight } from 'react-instantsearch-dom';
+import { 
+    InstantSearch,
+    SearchBox,
+    Hits,
+    Highlight,
+    Pagination,
+    ClearRefinements,
+    RefinementList,
+    Configure,
+    Snippet
+} from 'react-instantsearch-dom';
+import "instantsearch.css/themes/algolia-min.css";
 import { instantMeiliSearch } from '@meilisearch/instant-meilisearch';
 
 const searchClient = instantMeiliSearch(
-    process.env.HOST_NAME ?? '',
-    process.env.HOST_KEY ?? ''
+    process.env.REACT_APP_HOST_NAME ?? '',
+    process.env.REACT_APP_HOST_KEY ?? ''
 );
 
 const Home = () => (
-  <InstantSearch
-    indexName={process.env.INDEX_NAME}
-    searchClient={searchClient}
-  >
-    <SearchBox />
-    <Hits hitComponent={Hit} />
-  </InstantSearch>
+    <div className="ais-InstantSearch">
+        <h1>Engineering blogs</h1>
+        <InstantSearch indexName={process.env.REACT_APP_INDEX_NAME} searchClient={searchClient}>
+          <div className="left-panel">
+            <ClearRefinements />
+            <h2>Blogs</h2>
+            <RefinementList attribute="blogName" />
+            <Configure hitsPerPage={40} />
+          </div>
+          <div className="right-panel">
+            <SearchBox />
+            <Hits hitComponent={Hit} />
+            <Pagination />
+          </div>
+        </InstantSearch>
+      </div>
 );
 
-function Hit(props: any) {
-  return <Highlight attribute="name" hit={props.hit} />;
-}
+
+  function Hit(props) {
+    return (
+        <div key={props.hit.id}>
+            <div className="hit-name">
+                <Highlight attribute="title" hit={props.hit} />
+            </div>
+            <img src={props.hit.image} alt={props.hit.name} />
+            <div className="hit-description">
+                <Snippet attribute="description" hit={props.hit} />
+            </div>
+            <div className="hit-info">Blog: {props.hit.blogName}</div>
+            <div className="hit-info">Published: {props.hit.publish_date}</div>
+        </div>
+    );
+  }
 
 export default Home;
