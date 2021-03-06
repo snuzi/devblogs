a962e9b6e36d76b38e5a42cbd867fe0ca10c74b4
snuzi <sabrinuzi@gmail.com>
2021-03-06 13:31:19 +0100

    UI improvements, add published time to post item

--- src/components/Hit.tsx
+++ src/components/Hit.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { Highlight} from 'react-instantsearch-dom';
+import TimeAgo from 'javascript-time-ago';
+import en from 'javascript-time-ago/locale/en';
+import ReactTimeAgo from 'react-time-ago'
+
+TimeAgo.addLocale(en)
 
 export const Hit = (props) => {
+    const timeAgo = new TimeAgo('en-US');
     const { hit } = props; 
     const truncateWithEllipses = (text, max) => {
         return text.substr(0,max-1)+(text.length>max ? '...' : ''); 
     }
+    const publishDate = new Date(hit.publish_timestamp * 1000);
 
     return (
         <div className="col-sm-6">
@@ -18,16 +25,17 @@ export const Hit = (props) => {
                     {
                         props.hit.image && 
                         <a target={"_blank"} href={props.hit.link}>
-                            <img src={props.hit.image} title="" alt={props.hit.title}/>
+                            <img src={hit.image} title="" alt={props.hit.title}/>
                         </a>
                     }
                 </div>
                 <div className="blog-info">
-                    <h5><a href={props.hit.link}><Highlight attribute="title" hit={props.hit} /></a></h5>
-                    <p>{truncateWithEllipses(props.hit.description, 220)}</p>
+                    <h5><a href={hit.link}><Highlight attribute="title" hit={props.hit} /></a></h5>
+                    <p>{truncateWithEllipses(hit.description, 220)}</p>
                     <div className="btn-bar">
                         <span>{hit.blogName} - </span>
-                        <a target={"_blank"} href={props.hit.link} className="px-btn-arrow">
+                        <span key={hit.link}><ReactTimeAgo date={publishDate} locale="en-US"/> - </span>
+                        <a target={"_blank"} href={hit.link} className="px-btn-arrow">
                             <span>Read More</span> 
                             <i className="arrow"></i>
                         </a>
