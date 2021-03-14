88281bedf9c1788804d5d1ceb282d8b86414d449
snuzi <sabrinuzi@gmail.com>
2021-03-14 20:30:47 +0100

    Improve header

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container">
-                    <a className="navbar-brand" href="#">Engineering tech blogs</a>
+                    <a className="navbar-brand" href="#">Engineering Tech Blogs</a>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                         </button>
