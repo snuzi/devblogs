a65554623c9820867728d417c3d794c5e42d6ba9
snuzi <sabrinuzi@gmail.com>
2021-03-15 17:31:08 +0100

    Fix menu

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Home from './pages/Home';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
 
 function App() {
   return (
