f5593d500b01f6e5b413d7dccc93285d8686f1e3
snuzi <sabrinuzi@gmail.com>
2021-03-20 15:13:47 +0100

    Use G Analytics

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,12 +11,26 @@ import "instantsearch.css/themes/algolia-min.css";
 import { instantMeiliSearch } from '@meilisearch/instant-meilisearch';
 import InfiniteHits from '../components/InfinitHits';
 import { Hits } from '../components/Hits';
+import { createBrowserHistory } from 'history';
+import ReactGA from 'react-ga';
+
+const history = createBrowserHistory();
+
+ReactGA.initialize(process.env.REACT_APP_GANALYTICS_ID ?? '');
+
+ReactGA.set({ page: 'Home page' }); // Update the user's current page
+ReactGA.pageview('Home page'); // Record a pageview for the given page
+
+// Initialize google analytics page view tracking
+history.listen(location => {
+  ReactGA.set({ page: location.pathname }); // Update the user's current page
+  ReactGA.pageview(location.pathname); // Record a pageview for the given page
+});
 
 const searchClient = instantMeiliSearch(
     process.env.REACT_APP_HOST_NAME ?? '',
     process.env.REACT_APP_HOST_KEY ?? ''
 );
-
 
 const Home = () => (
     <section className="blog-listing gray-bg">
